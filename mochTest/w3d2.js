29ec8304fd36d8006cce620d66b36b9086bc961b
mft123eli <89947358+mft123eli@users.noreply.github.com>
2021-09-15 20:49:59 -0500

    Create w3d2.js

---
+++ mochTest/w3d2.js
@@ -0,0 +1,134 @@
+
+function TreeNode(value) {
+    this.value = value;
+    this.descendents = [];
+}
+   // create nodes with values
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+// associate root with is descendents
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
+//Question 1
+function printName(tree) {
+    if (tree.descendents === null) {
+        console.log("name:", tree.value)
+    }
+    else {
+        
+        console.log("name:", tree.value)
+        for (let child of tree.descendents) {
+            printName(child)
+        }
+    }
+}
+ 
+console.log(printName(abe));
+
+//Question 2
+function contains(tree,name){
+    if(tree.value===name)
+        return true;
+    if(tree.descendents && tree.descendents.length>0){
+        for(let item of tree.descendents){
+           if(contains(item,name))
+               return true;
+        }
+    }
+   return false;
+}
+
+console.log( contains(abe,"lisa"));
+
+//Question 3
+function getMatchingTree(tree,name){
+    if(tree.value===name){
+        return tree;
+    }
+    if(tree.descendents && tree.descendents.length>0){
+        for(let item of tree.descendents){
+           let matchingNode = getMatchingTree(item,name);
+           if(matchingNode){
+                return matchingNode; 
+           }
+        }
+    }
+    return null;
+}
+console.log("getMatchingTree ",getMatchingTree(abe,"Homer"));
+//  Question 4 Create a new constructor function ListNode (based on TreeNode below) and use it to generate a 
+// linked list of Abe, Homer, Bart, Lisa, Maggie instead of a tree.
+
+function ListNode(value,next){
+    this.value = value;
+   
+}
+// let lisNode = new ListNode(abe.value)
+// let maggieNode = new ListNode("Maggie",null);
+// let lisaNode = new ListNode("Lisa",maggieNode);
+// let bartNode = new ListNode("Bart",lisaNode);
+// let homerNode = new ListNode("Homer",bartNode);
+// let abeNode =  new ListNode("Abe",homerNode);
+
+
+function createlinkedList(abe){
+    
+let listNode = new ListNode(abe.value);
+
+listNode.next = new ListNode(homer.value);
+listNode.next.next = new ListNode(bart.value);
+listNode.next.next.next = new ListNode(lisa.value);
+listNode.next.next.next.next = new ListNode(maggie.value);
+console.log(JSON.stringify(listNode));
+}
+createlinkedList(abe);
+
+//question 5
+function targetValue(tree, name) {
+    if (tree.value === name) {
+        console.log(tree);
+    }
+    if (tree.descendents === null) return;
+    tree.descendents.forEach(child => targetValue(child, name));
+}
+ 
+let nameValue = "Abe";
+targetValue(abe, nameValue);
+
+//Question 6
+
+
+function treeModifier(tree, func) {
+        tree.value = func(tree);
+        if (tree.descendents === null) {
+     
+            return tree;
+        } else {
+            tree.value = func(tree);
+            for (let node of tree.descendents) {
+     
+                treeModifier(node, func);
+     
+            }
+        }
+        return tree
+    }
+     
+    // function allCaps(node) {
+    //    console.log(node.value = node.value.toUpperCase());
+    // }
+    function addStars(node) {
+        console.log("***" + node.value + "***");
+    }
+    // function reverseNode(node) {
+    //     console.log(node.value.split("").reverse().join(""));
+    // }
+    //treeModifier(abe, allCaps); //Cannot read property 'toUpperCase' of undefined
+    treeModifier(abe, addStars);
+    //treeModifier(abe, reverseNode); //Cannot read property 'split' of undefined
+    
+    
+
